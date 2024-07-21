8c4b990a8dc3ac1b626e94cf56dad4805edcd739
Brandon Boettcher <brandonboettcher19@gmail.com>
2024-07-21 01:54:06 -0500

    Add King, Knight, and Pawn attack functions to Attack.cpp

--- ChessEngine/Attack.h
+++ ChessEngine/Attack.h
@@ -7,6 +7,12 @@ namespace attack {
     void initializeBishopAttackTable();
     void initializeRookAttackTable();
 
+    uint64 getKingAttacks(uint64 king);
+    uint64 getKnightAttacks(int square);
+    uint64 getWhitePawnAttacksRight(uint64 pawns);
+    uint64 getWhitePawnAttacksLeft(uint64 pawns);
+    uint64 getBlackPawnAttacksRight(uint64 pawns);
+    uint64 getBlackPawnAttacksLeft(uint64 pawns);
     uint64 getBishopAttacks(int sq, uint64 allPieces);
     uint64 getRookAttacks(int sq, uint64 allPieces);
     uint64 getQueenAttacks(int sq, uint64 allPieces);
