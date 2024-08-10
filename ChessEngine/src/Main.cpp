a372226bcaed25ea27b07b7bbe27cbe6c838d112
Brandon Boettcher <brandonboettcher19@gmail.com>
2024-08-09 19:26:54 -0500

    Fix signed-unsigned conversion warnings

--- ChessEngine/src/Main.cpp
+++ ChessEngine/src/Main.cpp
@@ -7,7 +7,7 @@
 
 constexpr inline int VERSION_MAJOR = 1;
 constexpr inline int VERSION_MINOR = 1;
-constexpr inline int VERSION_PATCH = 4;
+constexpr inline int VERSION_PATCH = 5;
 
 
 static bool hasNextToken(std::stringstream& ss) {
@@ -39,21 +39,22 @@ static void uci() {
             ss >> tok;
             tokens.push_back(tok);
         }
-        if (tokens.size() == 0) {
+        int numTokens = (int) tokens.size();
+        if (numTokens == 0) {
             continue;
         }
         if (tokens[0] == "isready") {
-            assert(tokens.size() == 1);
+            assert(numTokens == 1);
             if (!initialized) {
                 engine.initialize();
                 initialized = true;
             }
             std::cout << "readyok" << std::endl;
         } else if (tokens[0] == "position") {
-            assert(tokens.size() > 1);
+            assert(numTokens > 1);
             int index = 1;
             if (tokens[index] == "fen") {
-                assert(tokens.size() >= 8);
+                assert(numTokens >= 8);
                 std::string fen;
                 for (int i = 0; i < 6; ++i) {
                     fen += tokens[++index] + " ";
@@ -64,10 +65,10 @@ static void uci() {
                 assert(tokens[index] == "startpos");
                 engine.setupBoard();
             }
-            if (tokens.size() > ++index) {
+            if (numTokens > ++index) {
                 assert(tokens[index] == "moves");
                 std::vector<std::string> moves;
-                while (++index < tokens.size()) {
+                while (++index < numTokens) {
                     moves.push_back(tokens[index]);
                 }
                 engine.makeMoves(moves);
@@ -75,27 +76,27 @@ static void uci() {
         } else if (tokens[0] == "go") {
             SearchInfo info;
             int index = 0;
-            while (++index < tokens.size()) {
+            while (++index < numTokens) {
                 if (tokens[index] == "depth") {
-                    assert(index + 1 < tokens.size());
+                    assert(index + 1 < numTokens);
                     info.maxDepth = std::stoi(tokens[++index]);
                 } else if (tokens[index] == "winc") {
-                    assert(index + 1 < tokens.size());
+                    assert(index + 1 < numTokens);
                     info.inc[WHITE] = std::stoi(tokens[++index]);
                 } else if (tokens[index] == "binc") {
-                    assert(index + 1 < tokens.size());
+                    assert(index + 1 < numTokens);
                     info.inc[BLACK] = std::stoi(tokens[++index]);
                 } else if (tokens[index] == "wtime") {
-                    assert(index + 1 < tokens.size());
+                    assert(index + 1 < numTokens);
                     info.time[WHITE] = std::stoi(tokens[++index]);
                 } else if (tokens[index] == "btime") {
-                    assert(index + 1 < tokens.size());
+                    assert(index + 1 < numTokens);
                     info.time[BLACK] = std::stoi(tokens[++index]);
                 } else if (tokens[index] == "movetime") {
-                    assert(index + 1 < tokens.size());
+                    assert(index + 1 < numTokens);
                     info.movetime = std::stoi(tokens[++index]);
                 } else if (tokens[index] == "movestogo") {
-                    assert(index + 1 < tokens.size());
+                    assert(index + 1 < numTokens);
                     info.movestogo = std::stoi(tokens[++index]);
                 }
             }
@@ -104,10 +105,10 @@ static void uci() {
             }
             searchThread = std::thread(search, std::ref(engine), info);
         } else if (tokens[0] == "stop") {
-            assert(tokens.size() == 1);
+            assert(numTokens == 1);
             engine.stopSearch();
         } else if (tokens[0] == "quit") {
-            assert(tokens.size() == 1);
+            assert(numTokens == 1);
             engine.stopSearch();
             break;
         }
@@ -123,9 +124,9 @@ int main() {
     std::cin >> protocol;
     if (protocol == "uci") {
         uci();
-    }
-    else {
+    } else {
         std::cout << "Error: unrecognized protocol" << std::endl;
+        std::cout << "Running perft tests instead." << std::endl;
         Engine engine;
         engine.initialize();
         engine.runPerftTests();
